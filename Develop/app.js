2c7b0d1639e603bf29d5f0c0dd6ba0ec0acfee8c
lucpizz <Lucpizz@gmail.com>
2021-01-18 21:15:06 -0500

    updated the inquier prompt of questions, create swtich for answers, and added write file to create team.html code

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,99 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const addEmployee = (employees = []) => {
+  console.log("Welcome to the Team Generator Program!");
+  console.table(employees);
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "list",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        name: "name",
+        type: "input",
+        message: "Please enter your name: ",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "Please enter your ID number: ",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Please enter your email address: ",
+      },
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "Please enter your office number: ",
+        when: (answers) => answers.title === `Manager`,
+      },
+      {
+        name: "github",
+        type: "input",
+        message: "Please enter your GitHub username: ",
+        when: (answers) => answers.title === `Engineer`,
+      },
+      {
+        name: "school",
+        type: "input",
+        message: "Please enter the name of your school: ",
+        when: (answers) => answers.title === `Intern`,
+      },
+      {
+        name: "continue",
+        type: "confirm",
+        message: "Would you like to add another employee?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      switch (answers.title) {
+        case "Manager":
+          employees.push(
+            new Manager(
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.officeNumber
+            )
+          );
+          break;
+
+        case "Engineer":
+          employees.push(
+            new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.github
+            )
+          );
+          break;
+        case "Intern":
+          employees.push(
+            new Intern(answers.name, answers.id, answers.email, answers.school)
+          );
+          break;
+        default:
+          return true;
+      }
+      if (answers.continue) {
+        addEmployee(employees);
+      } else {
+        fs.writeFile(outputPath, render(employees), () => {
+          console.log(`I put a file in ${outputPath}`);
+        });
+      }
+    });
+};
+
+addEmployee();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
